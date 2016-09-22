163df64fbc3786b91502df32e745f26f575a220e
esseswann <esseswann@gmail.com>
2016-09-22 15:40:33 +0300

    Examples updated with onClick prop proxying

--- examples/index.js
+++ examples/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom'
 
 const Title = ({attrs = [], styles = []}) =>
   <h3>
-    {'<'}<strong>Flex</strong> {attrs.map((attr, j) => <i key={j}>{attr} </i>)} >
+    {'<'}<strong>Flex</strong> {attrs.map((attr, j) => <div style={{marginLeft:'2ex'}} key={j}>{attr} </div>)} <span style={{paddingLeft:'2ex'}}>></span>
     {' = '}
     {styles.length === 0 ? 'default styles' : <span>{'{'} {styles.map((style) => style + '; ')}{'}'}</span>}
   </h3>
@@ -32,6 +32,16 @@ const Example = () =>
     <Title attrs={['hBetween', 'rowReverse']} styles={['justify-content: space-between', 'flex-direction: row-reverse']} />
     <div className = 'block'>
       <Flex hBetween rowReverse>
+        <div>Slaanesh</div>
+        <div>Tzeentch</div>
+        <div>Kharn</div>
+        <div>Nurgle</div>
+      </Flex>
+    </div>
+    <Title attrs={['hBetween', 'rowReverse', 'onClick = {() => alert("Hi!")}']} styles={['justify-content: space-between', 'flex-direction: row-reverse']} />
+
+    <div className = 'block'>
+      <Flex hBetween rowReverse onClick={() => alert('Hi!')}>
         <div>Slaanesh</div>
         <div>Tzeentch</div>
         <div>Kharn</div>
